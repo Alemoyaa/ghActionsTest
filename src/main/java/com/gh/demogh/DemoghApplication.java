d22fd215d57e555bd12025b54edff7383f23347d
Alejandro-Moya0 <alejandro.moya@sancorsalud.com.ar>
2022-05-13 18:33:49 -0300

    First

---
+++ src/main/java/com/gh/demogh/DemoghApplication.java
@@ -0,0 +1,13 @@
+package com.gh.demogh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoghApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoghApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gh/demogh/DemoghApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gh.demogh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoghApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
